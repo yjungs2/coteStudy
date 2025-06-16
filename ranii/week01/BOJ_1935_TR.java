29c48f9040341ad204765c5d7f99dd9c7fe22882
이태란 <dalbogi5@gmail.com>
2025-06-16 19:37:06 +0900

    delete : taeran page

---
+++ ranii/week01/BOJ_1935_TR.java
@@ -0,0 +1,11 @@
+package ranii.week01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_1935_TR {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
