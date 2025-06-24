2a84ceb169096ef140d3ecd6bcd57051eac9f457
MasterLee <39284552+yylee0102@users.noreply.github.com>
2025-06-24 10:59:09 +0900

    [PY] BOJ_2504_yy_02

--- yoon/week01/BOJ_2493_yy_02.py
+++ yoon/week01/BOJ_2493_yy_02.py
@@ -15,5 +15,5 @@ print(*answer)
 
 # stack 을 아직 해결하지 못한 index로 보고 풀었다.
 # deque는 pop() popleft() 가 모두 O(1) 이므로 적절하다고 보았다.
-# 그리고 print(*answer)은 리스트를 공백을 기준으로 한 칸 띄워서 출력한다는 의미입니다.
+# print(*answer)은 리스트를 공백을 기준으로 한 칸 띄워서 출력한다.
 # ex) print(*answer) ==> 출력 : 0 0 2 2 4
