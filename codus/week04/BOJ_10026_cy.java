bd4343fbd6a276afcd7fbf678480251b29065892
cheny <cheyeon001214@naver.com>
2025-07-21 18:24:44 +0900

    BOJ_10026 푸는 중

---
+++ codus/week04/BOJ_10026_cy.java
@@ -0,0 +1,22 @@
+package codus.week04;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10026_cy {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                
+            }
+        }
+
+    }
+}
