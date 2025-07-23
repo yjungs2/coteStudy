c0e32733f1e60f9c3b30cdc1f329b5ae6d97fc98
cheny <cheyeon001214@naver.com>
2025-07-23 19:44:46 +0900

    적록색약/골드5

--- codus/week03/BOJ_16918_cy.java
+++ codus/week03/BOJ_16918_cy.java
@@ -1,81 +1,75 @@
 package codus.week03;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class BOJ_16918_cy {
-    public static char arr[][] = null;
-    public static char list[][] = null;
-    static int R,C,N = 0;
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        //N이 짝수이면 보두 0 출력, 홀수이면 3일때-> 펑 5일때-> 원래대로 
-        list = new char[R][C];
-        arr = new char[R][C];
-        for(int i=0; i<R; i++){
-            String s = br.readLine();
-            for(int j=0; j<C; j++){
-                arr[i][j] = s.charAt(j);
+
+   static int N;
+   static char[][] chars;
+   static boolean[][] v;
+   
+   public static void main(String[] args) throws Exception{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      N = Integer.parseInt(br.readLine());
+      chars = new char[N][N];
+      v = new boolean[N][N];
+      
+      String S = null;
+      for(int i=0; i<N; i++) {
+         S = br.readLine();
+         for(int j=0; j<N; j++) {
+            chars[i][j] = S.charAt(j);
+         }
+      }
+      
+      
+      int n_count = 0;
+      for(int i=0; i<N; i++) {
+         for(int j=0; j<N; j++) {
+            if(v[i][j] != true) {
+               dfs(i, j);
+               n_count++;
             }
-        }
-        
-        //3,7,11,15,... -> (x-3)%4 == 0
-        //5,9,13,17,21... -> (x-5)%4 == 0
-        if (N == 1) {
-            list = arr;
-        } else if (N % 2 == 0) {
-            // 모든 칸 폭탄
-            for (int i = 0; i < R; i++) {
-                for (int j = 0; j < C; j++) {
-                    list[i][j] = 'O';
-                }
+         }
+      }
+      v = new boolean[N][N];
+      
+      int f_count = 0;
+      for(int i=0; i<N; i++) {
+         for(int j=0; j<N; j++) {
+            if(chars[i][j] == 'G') {
+               chars[i][j] = 'R';
             }
-        } else if ((N - 3) % 4 == 0) {
-            list = BFS(arr, new char[R][C]);
-        } else if ((N - 5) % 4 == 0) {
-            char[][] a = BFS(arr, new char[R][C]);
-            list = BFS(a, new char[R][C]);
-        }
-
-
-        for(int i=0; i<R; i++){
-            for(int j=0; j<C; j++){
-                System.out.print(list[i][j]);
+         }
+      }
+      
+      for(int i=0; i<N; i++) {
+         for(int j=0; j<N; j++) {
+            if(v[i][j] != true) {
+               dfs(i, j);
+               f_count++;
             }
-            System.out.println();
-        }
-    }
+         }
+      }
+      
+      System.out.println(n_count+" "+f_count);
+      
+   }
 
-    public static char[][] BFS(char[][] arr, char[][] list){
-        int a1[] = {1,-1,0,0};
-        int a2[] = {0,0,-1,1}; //(0,1), (0,-1), ... 상하좌우
-
-        for(int i=0; i<R; i++){
-            for(int j=0; j<C; j++){
-                list[i][j] = 'O';
+   private static void dfs(int i, int j) {
+      int dx[] = {0,0,1,-1};
+      int dy[] = {1,-1,0,0};
+      v[i][j] = true;
+      char temp = chars[i][j];
+      for(int k=0; k<4; k++) {
+         int x = i+ dx[k];
+         int y = j+ dy[k];
+         if(x>=0 && x<N && y <N && y >=0) {
+            if(!v[x][y] && chars[x][y] == temp) {
+               dfs(x, y);
             }
-        }
+         }
+      }
+   }
 
-        for(int i=0; i<R; i++){
-            for(int j=0; j< C; j++){
-                if(arr[i][j] == 'O'){
-                    list[i][j] = '.';
-                    for(int k=0; k<4; k++){
-                        int nx = i + a1[k];
-                        int ny = j + a2[k];
-                        if(nx >= 0 && nx < R && ny >=0 && ny<C){
-                            list[nx][ny] = '.';
-                        }
-                    }
-                }
-            }
-        }
-
-        return list;
-    }
 }
